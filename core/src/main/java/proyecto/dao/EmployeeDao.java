da58413fbcd85c9c4a5ff54bf68b98eaedd82f45
Gui11erm0 <guillespad@gmail.com>
2015-12-14 18:16:49 -0600

    go

---
+++ core/src/main/java/proyecto/dao/EmployeeDao.java
@@ -0,0 +1,12 @@
+package proyecto.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+import proyecto.model.Employee;
+
+public interface EmployeeDao extends GenericDao<Employee, Long> {
+	
+	public List<Employee> getAll();
+
+}
